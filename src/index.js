1757f668bdb51a50755d3c24deb3abaf135240d8
Bryce J. Meyer <bjm904@gmail.com>
2023-01-29 16:41:40 -0800

    Make API search return correctly

--- src/index.js
+++ src/index.js
@@ -6,29 +6,47 @@ const apiBaseUrl = 'https://derpibooru.org/api/v1/json';
 
 const images = [];
 
-axios.get(`${apiBaseUrl}/search/images`, {
-    params: {
-        per_page: 50,
-        q: 'pinkie pie',
-        page: 0,
-        sd: '',
-        sf: '',
-    }
-    })
-    .then((response) => {
-        if (response.status === 200) {
-            if (response.data.images.length > 0) {
-            const newImages = response.data.images.map((apiImage) => ({ link: apiImage.view_url, imgSrc: apiImage.respresentations.medium, api: apiImage }));
-            images.push(...newImages);
+const renderImages = (firstRender) => {
+  if (firstRender) {
     
-            console.log(response);
-            } else {
-                console.error('Received no images from search');
-            }
-        } else {
-            console.error(`Received response code ${response.status}`);
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+  }
+};
+
+const initialize = () => {
+  const params = {
+    per_page: 50,
+    q: 'pinkie pie',
+    page: 0,
+    sd: '',
+    sf: '',
+  };
+
+  axios.get(`${apiBaseUrl}/search/images`, { params }).then((response) => {
+    if (response.status === 200) {
+      if (response.data.images.length > 0) {
+      const newImages = response.data.images.map((apiImage) => {
+        if (!apiImage) throw new Error();
+        return {
+          link: apiImage.view_url,
+          imgSrc: apiImage.representations.medium,
+          api: apiImage,
+        };
+      });
+      images.push(...newImages);
+
+      renderImages(true);
+
+      console.log(response);
+      } else {
+          console.error('Received no images from search');
+      }
+    } else {
+        console.error(`Received response code ${response.status}`);
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+};
+
+initialize();
